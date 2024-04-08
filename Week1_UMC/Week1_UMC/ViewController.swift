088866ba7ad271f573e85c947dd8341ec9cd3dee
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2024-04-08 11:03:21 +0900

    Feat: 2주차 워크북 미션

--- Week1_UMC/Week1_UMC/ViewController.swift
+++ Week1_UMC/Week1_UMC/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var normalDeliveryBtn: UIButton!
     @IBOutlet weak var bmartBtn: UIButton!
     
+    @IBOutlet weak var cornerView: UIView!
+    
+    @IBOutlet weak var StackLabel1: UILabel!
+    @IBOutlet weak var StackLabel2: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +31,23 @@ class ViewController: UIViewController {
 
     func setUI() {
         topBtn.setTitle("집콕러세요? 30% 쿠폰 드려요!", for: .normal)
+        deliveryBtn.setTitle("알뜰배달", for: .normal)
+        deliveryBtn.titleLabel?.font = UIFont(name: "Impact", size: 24)
+        normalDeliveryBtn.setTitle("배달", for: .normal)
+        normalDeliveryBtn.titleLabel?.font = UIFont(name: "Impact", size: 24)
+        bmartBtn.setTitle("배민스토어", for: .normal)
+        bmartBtn.titleLabel?.font = UIFont(name: "Impact", size: 24)
+        
+        StackLabel1.text = "대용량특가"
+        StackLabel1.font = UIFont(name: "Impact", size: 25)
+        StackLabel2.text = "많이 살수록 더 저렴하죠"
+        
+        adButton.setTitle("신전떡볶이 특급 할인", for: .normal)
+        adButton.titleLabel?.font = UIFont(name: "Impact", size: 24)
+        
+        cornerView.clipsToBounds = true
+        cornerView.layer.cornerRadius = 20
+        cornerView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner)
     }
 }
 
