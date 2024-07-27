ca8f05c46723208500c3dcb3987057f10cb2c9dd
Hyeon-Ju <lhjivy1024@gmail.com>
2024-07-28 00:24:50 +0900

    .git 제거

---
+++ Desktop/umc_6th_ios/week1/calculator/calculator/Roundbutton.swift
@@ -0,0 +1,16 @@
+//  RoundButton.swift
+
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+}
+
